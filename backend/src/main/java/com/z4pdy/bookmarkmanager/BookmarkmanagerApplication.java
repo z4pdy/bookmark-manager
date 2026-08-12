ef79ded336060a4596d764797ba04ee4f2ddbd09
z4pdy <305461556+z4pdy@users.noreply.github.com>
2026-08-12 15:33:11 +0200

    chore(backend): initialize project

---
+++ backend/src/main/java/com/z4pdy/bookmarkmanager/BookmarkmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.z4pdy.bookmarkmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkmanagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/z4pdy/bookmarkmanager/BookmarkmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.z4pdy.bookmarkmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
